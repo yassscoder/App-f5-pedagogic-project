608e2456261e76d2fbf567731eda4276d48b6eec
yassscoder <yasminlcg@gmail.com>
2021-08-23 14:31:04 +0100

    Initial commit

---
+++ src/main/java/org/factoriaf5/appf5/Appf5Application.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.appf5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appf5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Appf5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/appf5/Appf5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.appf5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appf5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
