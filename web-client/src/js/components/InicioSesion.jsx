87fbd9ad64396abcd2f652851a647bbdcb83bd5b
Ana Lissette <lissilab@hotmail.com>
2021-08-26 13:01:46 +0200

    Inicio Sesion primera face

---
+++ web-client/src/js/components/InicioSesion.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import "../../scss/InicioSesionStyles.scss";
+import fondo from "./assets/fondoF5.jpg";
+import fondoNaranja from "./assets/logoNaranjaF5.png";
+import usuario from "./assets/usuario.png";
+import candado from "./assets/candado.png";
+
+export const InicioSesion = () => (
+
+    <div className={"inicioSesion-main"}>
+        <img className={"inicioSesion-main__fondo"} src={fondo}/>
+        <div className={"inicioSesion-main__item"}>
+            <img className={"inicioSesion-main__logoN"} src={fondoNaranja}/>
+        </div>
+        <div className={"inicioSesion-main__item"}>
+            <span>INICIAR SESIÓN</span>
+            <section>
+                <img src={usuario}/>
+                <input/>
+            </section>
+            <section>
+                <img src={candado}/>
+                <input/>
+            </section>
+            <button></button>
+        </div>
+    </div>
+
+)
