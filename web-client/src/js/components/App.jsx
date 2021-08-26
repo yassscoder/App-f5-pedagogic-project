f7102b761f038998721bf5981d00f5f31e65e0de
yassscoder <yasminlcg@gmail.com>
2021-08-26 09:50:37 +0200

    cambios colores root, posicionamiento

--- web-client/src/js/components/App.jsx
+++ web-client/src/js/components/App.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {NavBar} from "./NavBar";
 import {Footer} from "./Footer";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import "../../scss/main.scss";
 
 
 export const App = () => {
