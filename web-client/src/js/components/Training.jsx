f875457164fc3d97c470e1a632ba655468c243dd
Alisa <filonenkoalisa@gmail.com>
2021-09-01 10:54:32 +0200

    Training structura

---
+++ web-client/src/js/components/Training.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import "../../scss/Training.scss";
+import fondoNaranja from "./assets/logoNaranjaF5.png";
+
+
+export const Training = () => (
+
+    <div className="card">
+        <div className="card-top">
+            <h2>Barcelona</h2>
+        </div>
+        {/*<div className="card-content">
+            <div className="subtext"></div>
+        </div>*/}
+
+        <div className="card-down">
+            <div className="subtext-down">
+                <h1>Full Stack</h1>
+                <h2>850 h</h2>
+            </div>
+        </div>
+    </div>
+)
+
