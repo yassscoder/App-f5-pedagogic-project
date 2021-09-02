fa58b47e684516aca677e2ed234f8d3926155655
yassscoder <yasminlcg@gmail.com>
2021-09-02 09:40:41 +0200

    cambios alineacion y tamaños en rem de tarjetas formacion

--- web-client/src/js/components/Training.jsx
+++ web-client/src/js/components/Training.jsx
@@ -4,15 +4,48 @@ import {Link} from "react-router-dom";
 
 
 export const Training = () => (
+    <div className={"container-cards"}>
+        <div className="card__header">
+            <div className="card__title">
+                <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+            </div>
 
-    <div className="card__header">
-        <div className="card__title">
-            <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+            <div className="card__subtitle">
+                <h1 id="courses">FULL STACK</h1>
+                <h2 id="durations">850 H</h2>
+            </div>
         </div>
 
-        <div className="card__subtitle">
-            <h1 id="courses">FULL STACK</h1>
-            <h2 id="durations">850 H</h2>
+        <div className="card__header">
+            <div className="card__title">
+                <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+            </div>
+
+            <div className="card__subtitle">
+                <h1 id="courses">FULL STACK</h1>
+                <h2 id="durations">850 H</h2>
+            </div>
+        </div>
+        <div className="card__header">
+            <div className="card__title">
+                <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+            </div>
+
+            <div className="card__subtitle">
+                <h1 id="courses">FULL STACK</h1>
+                <h2 id="durations">850 H</h2>
+            </div>
+        </div>
+
+        <div className="card__header">
+            <div className="card__title">
+                <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+            </div>
+
+            <div className="card__subtitle">
+                <h1 id="courses">FULL STACK</h1>
+                <h2 id="durations">850 H</h2>
+            </div>
         </div>
     </div>
 )
