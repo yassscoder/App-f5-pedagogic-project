ab9b98a95c497a5e19ac42ce0b03c21d70dd2a01
Ana Lissette <lissilab@hotmail.com>
2021-09-02 10:59:37 +0200

    editando cards

--- web-client/src/js/components/Training.jsx
+++ web-client/src/js/components/Training.jsx
@@ -7,6 +7,27 @@ export const Training = () => (
     <div className={"container-cards"}>
         <div className="card__header">
             <div className="card__title">
+                <Link to={"/"} className={"card__title-main"} id="cities">Gijon</Link>
+            </div>
+
+            <div className="card__subtitle">
+                <h1 id="courses">FULL STACK</h1>
+                <h2 id="durations">850 H</h2>
+            </div>
+        </div>
+
+        <div className="card__header">
+            <div className="card__title">
+                <Link to={"/"} className={"card__title-main"} id="cities">Madrid</Link>
+            </div>
+
+            <div className="card__subtitle">
+                <h1 id="courses">FULL STACK</h1>
+                <h2 id="durations">850 H</h2>
+            </div>
+        </div>
+        <div className="card__header">
+            <div className="card__title">
                 <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
             </div>
 
@@ -22,29 +43,29 @@ export const Training = () => (
             </div>
 
             <div className="card__subtitle">
-                <h1 id="courses">FULL STACK</h1>
-                <h2 id="durations">850 H</h2>
+                <h1 id="courses">Front-End</h1>
+                <h2 id="durations">300 H</h2>
             </div>
         </div>
         <div className="card__header">
             <div className="card__title">
-                <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+                <Link to={"/"} className={"card__title-main"} id="cities">Bilbao</Link>
             </div>
 
             <div className="card__subtitle">
                 <h1 id="courses">FULL STACK</h1>
-                <h2 id="durations">850 H</h2>
+                <h2 id="durations">1000 H</h2>
             </div>
         </div>
 
         <div className="card__header">
             <div className="card__title">
-                <Link to={"/"} className={"card__title-main"} id="cities">Barcelona</Link>
+                <Link to={"/"} className={"card__title-main"} id="cities">Sevilla</Link>
             </div>
 
             <div className="card__subtitle">
                 <h1 id="courses">FULL STACK</h1>
-                <h2 id="durations">850 H</h2>
+                <h2 id="durations">1000 H</h2>
             </div>
         </div>
     </div>
