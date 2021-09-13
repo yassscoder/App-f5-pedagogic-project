7c66b6d0e80f3a426c1109079ece63c65d944644
Olga Caparros <caparrosolga@gmail.com>
2021-09-13 10:34:50 +0200

    crear services

---
+++ src/main/java/org/factoriaf5/appf5/service/LessonIdService.java
@@ -0,0 +1,11 @@
+package org.factoriaf5.appf5.service;
+
+import java.util.List;
+
+public class LessonIdService {
+    public boolean isSelectedLesson(String id){
+        List <String> selectedIds = List.of("56533eb9ac21ba0edf2244a8", "bd7123c9c441eddfaeb4bdef"); //lista ejercicios
+        return
+                selectedIds.contains(id);
+    }
+}
