83b9a78c019f42c9b4f3d51770d852916d429b72
yassscoder <yasminlcg@gmail.com>
2021-09-15 21:31:07 +0200

    validación básica formulario candidatos

--- web-client/src/js/components/CandidateForm.jsx
+++ web-client/src/js/components/CandidateForm.jsx
@@ -3,6 +3,72 @@ import {useState} from "react";
 import {Redirect} from "react-router-dom";
 import {Formik, Field, Form} from "formik";
 
+function validateNameOrLastName(value) {
+    let error;
+
+    if (!value) {
+
+        error = 'Campo obligatorio';
+
+    } else if (!/^[a-zA-z]{2,64}$/i.test(value)) {
+
+        error = 'El campo ha de contener al menos 2 letras';
+
+    }
+
+    return error;
+
+}
+function validateAge(value) {
+    let error;
+
+    if (!value) {
+
+        error = 'Campo obligatorio';
+
+    } else if (!/^(?:1[01][0-9]|100|1[6-9]|[2-9][0-9])$/i.test(value)) {
+
+        error = 'La edad debe estar comprendida entre 16-100';
+
+    }
+
+    return error;
+
+}
+function validateMail(value) {
+    let error;
+
+    if (!value) {
+
+        error = 'Campo obligatorio';
+
+    } else if (!/^[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}$/i.test(value)) {
+
+        error = 'Ha de ser una dirección válida';
+
+    }
+
+    return error;
+
+}
+function validateUser(value) {
+    let error;
+
+    if (!value) {
+
+        error = 'Campo obligatorio';
+
+    } else if (!/^[\w\-]{3,64}$/i.test(value)) {
+
+        error = 'Caracteres permitidos letras de la A-Z, números del 1-9 y el signo -';
+
+    }
+
+    return error;
+
+}
+
+
 export const CandidateForm = (props) => {
     const [redirect, setRedirect] = useState (false);
 
@@ -27,17 +93,22 @@ export const CandidateForm = (props) => {
                             .then(_ => setRedirect(true))
                 }
             >
-                {({values}) => <Form>
+                {({errors, touched}) => <Form>
                     <label htmlFor="name">Nombre</label>
-                    <Field id="name" name="name"/>
+                    <Field id="name" name="name" validate={validateNameOrLastName}/>
+                    {errors.name && touched.name && <div>{errors.name}</div>}
                     <label htmlFor="lastName">Apellido</label>
-                    <Field id="lastName" name="lastName"/>
+                    <Field id="lastName" name="lastName" validate={validateNameOrLastName}/>
+                    {errors.lastName && touched.lastName && <div>{errors.lastName}</div>}
                     <label htmlFor="age">Edad</label>
-                    <Field id="age" name="age"/>
-                    <label htmlFor="mail">Correo electronico</label>
-                    <Field id="mail" name="mail"/>
-                    <label htmlFor="userFree">Usuario FreeCodeCamp</label>
-                    <Field id="userFree" name="userFree"/>
+                    <Field id="age" name="age" validate={validateAge}/>
+                    {errors.age && touched.age && <div>{errors.age}</div>}
+                    <label htmlFor="mail">Dirección e-mail </label>
+                    <Field id="mail" name="mail" validate={validateMail}/>
+                    {errors.mail && touched.mail && <div>{errors.mail}</div>}
+                    <label htmlFor="userFree"> Nombre usuario Free Code Camp</label>
+                    <Field id="userFree" name="userFree" validate={validateUser}/>
+                    {errors.userFree}
                     <button type={"submit"}>Guardar datos</button>
 
                 </Form>}
