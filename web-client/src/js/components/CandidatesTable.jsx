40824bf97aa31f2334d2c1e9872a5f9a7e595d0d
yassscoder <yasminlcg@gmail.com>
2021-09-18 23:35:57 +0200

    añadido material ui y cambios en diseño tabla candidatos

--- web-client/src/js/components/CandidatesTable.jsx
+++ web-client/src/js/components/CandidatesTable.jsx
@@ -1,18 +1,26 @@
 import * as React from "react";
+import { AccountCircle} from '@material-ui/icons';
+import {Box, Grid} from "@material-ui/core";
 
+
+// const MyAccountIcon = styled(AccountCircle)({
+//     color:'white'
+// });
 export const CandidatesTable = (props) =>
 
-    <div>
-        <div className={"table__content"}>
 
-            <div className={"table__row"}>
-                <div className={"table__info"} >{ props.candidate.name}</div>
-                <div className={"table__info"} >{ props.candidate.lastName}</div>
-                <div className={"table__info"} >{ props.candidate.age}</div>
-                <div className={"table__info"} >{ props.candidate.mail}</div>
-                <div className={"table__info"} >{ props.candidate.userFree}</div>
-            </div>
-        </div>
-    </div>
+                <tr className={"table__row"}>
+                    <td className={"table__info table__info--flex"} >{ props.candidate.name} { props.candidate.lastName}
+                        <a className={"table__acount"} href={"https://www.freecodecamp.org/" + `${props.candidate.userFree}` } target="_blank" >
+                             <AccountCircle className={"table__icon"}/> </a></td>
+                    <td className={"table__info"} >{ props.candidate.age}</td>
+                    <td className={"table__info"} >{ props.candidate.mail}</td>
+                    <td className={"table__info"} >{ props.candidate.userFree}</td>
+            </tr>
+
+
+
+
+
 
 
