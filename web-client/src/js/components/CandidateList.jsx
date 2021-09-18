7c8e0d0384479cf960e12f3f5f05525600dd5cac
yassscoder <yasminlcg@gmail.com>
2021-09-19 00:10:35 +0200

    añadido tooltip icon

--- web-client/src/js/components/CandidateList.jsx
+++ web-client/src/js/components/CandidateList.jsx
@@ -6,15 +6,15 @@ import {CandidatesTable} from "./CandidatesTable";
 export const CandidateList = (props) => (
 
     <table className={"table"}>
-    <thead>
+        <thead>
         <tr className={"table__header"}>
-            <th className={"table__info table__info--bold"} >Aspirante</th>
-            <th className={"table__info  table__info--bold"} >EDAD</th>
-            <th className={"table__info table__info--bold"} >EMAIL</th>
-            <th className={"table__info table__info--bold"} >USUARIO FREECODECAMP</th>
+            <th className={"table__info table__info--bold"}>Aspirante</th>
+            <th className={"table__info  table__info--bold"}>EDAD</th>
+            <th className={"table__info table__info--bold"}>EMAIL</th>
+            <th className={"table__info table__info--bold"}>USUARIO FREECODECAMP</th>
         </tr>
 
-    </thead>
+        </thead>
         <tbody className={"table__body"}>
         {props.candidates.map(candidate =>
             <CandidatesTable candidate={candidate}/>
--- web-client/src/js/components/CandidatesTable.jsx
+++ web-client/src/js/components/CandidatesTable.jsx
@@ -1,25 +1,21 @@
 import * as React from "react";
-import { AccountCircle} from '@material-ui/icons';
-import {Box, Grid} from "@material-ui/core";
+import {AccountCircle} from '@material-ui/icons';
+
+export const CandidatesTable = (props) => (
 
 
-// const MyAccountIcon = styled(AccountCircle)({
-//     color:'white'
-// });
-export const CandidatesTable = (props) =>
+    <tr className={"table__row"}>
+        <td className={"table__info table__info--flex"}>{props.candidate.name} {props.candidate.lastName}
+            <a href={"https://www.freecodecamp.org/" + `${props.candidate.userFree}`} title="Perfil FreeCodeCamp"
+               target="_blank">
+                <AccountCircle className={"table__icon"}/> </a></td>
+        <td className={"table__info"}>{props.candidate.age}</td>
+        <td className={"table__info"}>{props.candidate.mail}</td>
+        <td className={"table__info"}>{props.candidate.userFree}</td>
+    </tr>
 
 
-                <tr className={"table__row"}>
-                    <td className={"table__info table__info--flex"} >{ props.candidate.name} { props.candidate.lastName}
-                        <a className={"table__acount"} href={"https://www.freecodecamp.org/" + `${props.candidate.userFree}` } target="_blank" >
-                             <AccountCircle className={"table__icon"}/> </a></td>
-                    <td className={"table__info"} >{ props.candidate.age}</td>
-                    <td className={"table__info"} >{ props.candidate.mail}</td>
-                    <td className={"table__info"} >{ props.candidate.userFree}</td>
-            </tr>
-
-
-
+);
 
 
 
