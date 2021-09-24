dfd16037d1fb132af9f32a373c29f85907982c9d
yassscoder <yasminlcg@gmail.com>
2021-09-24 16:49:08 +0200

    añadido estilos editar y borrar en trainings

--- web-client/src/js/components/TrainingCard.jsx
+++ web-client/src/js/components/TrainingCard.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
-import ListRoundedIcon from '@mui/icons-material/ListRounded';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 
 
 export const TrainingCard = (props) =>
@@ -12,10 +13,16 @@ export const TrainingCard = (props) =>
 
             <h3 className={"card__info"}>{props.training.promoName}</h3>
             <h3 className={"card__info"}>{props.training.duration}h</h3>
-            <div>
+            <div className={"card__buttons__wrapper"}>
                 <Link className="card__button" to={"/Candidate-list"}>Aspirantes</Link>
+                <div className={"card__icons"}>
+                    <DeleteForeverOutlinedIcon className="card__delete"/>
+                    <EditOutlinedIcon className={"card__edit"}/>
+                </div>
+
             </div>
 
         </div>
     </div>
+
 
