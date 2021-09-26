32c9e4f3f90a0e0356e359336455fa7f6a6db362
Olga Caparros <caparrosolga@gmail.com>
2021-09-26 21:12:33 +0200

    añadidos más cuusosId en LessonService y usuario realen dataloader en lugar de imaginario

--- src/main/java/org/factoriaf5/appf5/configuration/DataLoader.java
+++ src/main/java/org/factoriaf5/appf5/configuration/DataLoader.java
@@ -31,12 +31,12 @@ public class DataLoader {
         List<Candidate> candidates = List.of(
                 new Candidate("Olga", "Caparros", 46, "olga@capa.com", "yassscoder", "3", "1", "0"),
                 new Candidate("Alisa", "Maravillosa", 35, "alisa@malla.com", "fcccc63c19d-3f28-486a-86c2-d614de411e74", "13", "2", "3")
-//                new Candidate(3L, "Yas", "Caparros", 33, "yas@capa.com", "user05", "3", "1", "2")
+//                new Candidate(3L, "Yas", "Caparros", 33, "yas@capa.com", "olgafemtech1", "3", "1", "2")
         );
         List<Candidate> candidates1 = List.of(
                 new Candidate("Olga", "Caparros", 46, "olga@capa.com", "yassscoder", "3", "1", "0"),
                 new Candidate("Alisa", "Maravillosa", 35, "alisa@malla.com", "fcccc63c19d-3f28-486a-86c2-d614de411e74", "13", "2", "3"),
-                new Candidate( "Yas", "Caparros", 33, "yas@capa.com", "user05", "3", "1", "2")
+                new Candidate( "Yas", "Caparros", 33, "yas@capa.com", "olgafemtech1", "3", "1", "2")
         );
 
         Training training = new Training(1L, "Barcelona", "Femtech P1", 850, candidates);
--- src/main/java/org/factoriaf5/appf5/service/LessonIdService.java
+++ src/main/java/org/factoriaf5/appf5/service/LessonIdService.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class LessonIdService {
 
-        private List <String> javaScriptExercises = List.of("56533eb9ac21ba0edf2244a8", "bd7123c9c441eddfaeb4bdef"); //lista ejercicios
-        private List <String> htmlExercises = List.of("5664820f61c48e80c9fa476c", "56533eb9ac21ba0edf2244dc");
-        private List <String> cssExercises = List.of("56533eb9ac21ba0edf2244ae", "bd7993c9ca9feddfaeb7bdef");
+        private List <String> javaScriptExercises = List.of("cf1111c1c11feddfaeb8bdef", "cf1111c1c11feddfaeb3bdef", "cf1111c1c11feddfaeb6bdef", "cf1111c1c11feddfaeb4bdef", "cf1111c1c11feddfaeb7bdef"); //lista ejercicios
+        private List <String> htmlExercises = List.of("5664820f61c48e80c9fa476c", "56533eb9ac21ba0edf2244a8", "56533eb9ac21ba0edf2244dc", "56533eb9ac21ba0edf2244d0", "56533eb9ac21ba0edf2244ae");
+        private List <String> cssExercises = List.of("bad82fee1348bd9aedf08721", "bd7123c9c441eddfaeb4bdef", "bd7993c9ca9feddfaeb7bdef", "bad87fee1348bd9aedf08726", "bad87fee1348bd9aedf06756");
 
     public List<String> getJavaScriptExercises() {
         return javaScriptExercises;
